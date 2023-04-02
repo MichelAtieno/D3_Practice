8e6323151227f4f32ace452849ba633de2ce2cfd
MichelAtieno <michelatieno23@gmail.com>
2023-04-02 13:45:50 +0300

    Added more test practice on bar charts

---
+++ index3.js
@@ -0,0 +1,16 @@
+d3.json("./sample.json")
+  .then(
+        function(d) {
+            console.log(d);
+        }
+    );
+
+// function main() {
+//     d3
+//      .json("./sample.json")
+//      .then(
+//         function(d) {
+//             console.log(d);
+//         }
+//      )
+// }
